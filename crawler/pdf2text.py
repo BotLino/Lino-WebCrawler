090c209d599f86971edcfea9a748a254da65c5bc
guilacerda <guiglacerda@gmail.com>
2019-05-31 19:57:51 -0300

    Creates method to parser pdf to text
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- crawler/pdf2text.py
+++ crawler/pdf2text.py
@@ -2,26 +2,20 @@ from tika import parser
 import os
 
 
-def extract_text_from_pdfs_recursively(dir):
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            path_to_pdf = os.path.join(root, file)
-            [stem, ext] = os.path.splitext(path_to_pdf)
-            if ext == '.pdf':
-                print("Processing " + path_to_pdf)
-                pdf_contents = parser.from_file(path_to_pdf)
-                path_to_txt = stem + '.txt'
-                with open(path_to_txt, 'w') as txt_file:
-                    print("Writing contents to " + path_to_txt)
-                    txt_file.write(pdf_contents['content'])
+def extract_text_from_pdfs_recursively(path_to_pdf):
+    path_to_txt = ""
+    [stem, ext] = os.path.splitext(path_to_pdf)
+
+    if ext == '.pdf':
+        print("Processing " + path_to_pdf)
+        pdf_contents = parser.from_file(path_to_pdf)
+        path_to_txt = stem + '.txt'
+        with open(path_to_txt, 'w') as txt_file:
+            print("Writing contents to " + path_to_txt)
+            txt_file.write(pdf_contents['content'])
+
+    return path_to_txt
 
 
-if __name__ == "__main__":
-    extract_text_from_pdfs_recursively(os.getcwd())
-
-
-def creates_table():
-    file = open("teste.txt", 'r')
-    stringzona = ""
-    for line in file:
-        stringzona.append(line)
+# if __name__ == "__main__":
+#     extract_text_from_pdfs_recursively(os.getcwd())
