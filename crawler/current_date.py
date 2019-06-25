15ce18b9bc43199bcca2c18d29094fa1529df1e7
guilacerda <guiglacerda@gmail.com>
2019-06-25 10:56:34 -0300

    Changes generic name of variables to significant names

--- crawler/current_date.py
+++ crawler/current_date.py
@@ -4,13 +4,13 @@ from datetime import timedelta, datetime
 def get_first_day_week(signal):
     date_format = '%d' + signal + '%m' + signal + '%Y'
 
-    today = datetime.today().date()
-    today = today + timedelta(days=1)
-    today = today.strftime('%d/%m/%Y')
+    current_day = datetime.today().date()
+    next_day = current_day + timedelta(days=1)
+    correct_format_next_day = next_day.strftime('%d/%m/%Y')
 
-    dt = datetime.strptime(today, '%d/%m/%Y')
+    datetime_format = datetime.strptime(correct_format_next_day, '%d/%m/%Y')
 
-    start = dt - timedelta(days=dt.weekday())
-    start = start.strftime(date_format)
+    monday_date = datetime_format - timedelta(days=datetime_format.weekday())
+    monday_date = monday_date.strftime(date_format)
 
-    return start
+    return monday_date
